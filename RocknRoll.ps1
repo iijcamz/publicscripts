9fdb521c2f19b009891e78ea37853e2db940183f
Jun <junrey@geidi.com>
2022-09-16 13:04:17 +0800

    Updating code

--- RocknRoll.ps1
+++ RocknRoll.ps1
@@ -1,2 +1,2 @@
 mkdir C:\RocknRoll
-Invoke-WebRequest -Uri "https://www.dropbox.com/s/pfvdf994z92lomr/Warrikal-PC%20Provisioning%20Version%201.5.exe?dl=1" -OutFile C:\RocknRoll
+Invoke-WebRequest -Uri "https://www.dropbox.com/s/pfvdf994z92lomr/Warrikal-PC%20Provisioning%20Version%201.5.exe?dl=1" -OutFile "C:\RocknRoll\Warrikal-PC Provisioning.exe"
